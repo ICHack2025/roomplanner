13a62b7000125dacd126f4561c539a05d31dde18
Lorand Webber <lw2223@ic.ac.uk>
2025-02-02 09:30:21 +0000

    feat: added sleek design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
                   <div className="content pl-3 is-flex  is-align-items-center is-justify-content-center is-flex-direction-column">
                     <h1
                       style={{
-                        fontSize: "6em",
+                        fontSize: "10em",
                         fontWeight: "bold",
                         color: "black",
                       }}
@@ -23,7 +23,7 @@ const Home = () => {
                       Your <strong>personalized</strong> interior designing
                       assistant.
                     </p>
-                    <a href="/form" className="button is-large">
+                    <a href="/create_points" className="button is-large">
                       <span className="icon">
                         {/* <FontAwesomeIcon icon={faRocket} /> */}
                       </span>
@@ -46,7 +46,7 @@ const Home = () => {
           width: "100vw",
           height: "100vh",
           objectFit: "cover",
-          opacity: 0.2,
+          opacity: 0.05,
           zIndex: 0,
           pointerEvents: "none",
         }}
