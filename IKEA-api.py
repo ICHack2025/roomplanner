4de6874313188f45bd57eb5e8cc870819f09730b
hh2023 <helen.huang23@imperial.ac.uk>
2025-02-02 02:58:08 +0000

    refactor: cleans up!

--- IKEA-api.py
+++ IKEA-api.py
@@ -6,6 +6,7 @@ import zipfile
 import cv2
 import numpy
 from colorthief import ColorThief
+import shutil
 
 
 # Constants like country, language, base url
@@ -31,9 +32,11 @@ try:
         glb_url = None
         
         for model in variation['models']:
+
             # Check if the model is of format "usdz"
             if model['format'] == 'usdz':
                 usdz_url = model['url']
+
             # Check if the model is of format "glb_draco"
             elif (model['format'] == 'glb_draco') or (model['format'] == 'glb') :
                 glb_url = model['url']
@@ -45,17 +48,20 @@ try:
                 usdz_url = None
                 glb_url = None
 
-    # Print the set of pairs
-    print(model_urls)
-
-
 
     models_and_colors = []
 
+    # Temp dir to store base colors
+    base_colors = "base_colors"
+    if not os.path.exists(base_colors):
+        os.mkdir(base_colors)
+        print(f"Directory '{base_colors}' created.")
+    else:
+        print(f"Directory '{base_colors}' already exists.")
 
     for i, model_url_pair in enumerate(model_urls):
-        # print(f"\n3D Model Found: {model_url}")
 
+        # Seperate the pairs
         model_url_usdz = model_url_pair[0]
         model_url_glb = model_url_pair[1]
 
@@ -68,7 +74,7 @@ try:
             model_filename_usdz = model_url_usdz.split('/')[-1]
             model_filename_glb = model_url_glb.split('/')[-1]
                 
-            # print(f"model filename: {model_filename}")
+            # Saving the models as files
             with open(model_filename_usdz, "wb") as file:
                 file.write(model_response_usdz.content)
             print(f"3D model saved as: {model_filename_usdz}\n")
@@ -76,28 +82,27 @@ try:
                 file.write(model_response_glb.content)
             print(f"3D model saved as: {model_filename_glb}\n")
 
-            # FOR THE USDZ FILE
-            # if ".usdz" in model_filename: 
+            # usdz becomes ZIP to extract colors!
             model_filename_zip = (f"{i}.zip"
-                                    f"")  # Adjust file format if needed
+                                  f"")  # Adjust file format if needed
             with open(model_filename_zip, "wb") as file:
                 file.write(model_response_usdz.content)
 
-            # Path to the ZIP file
             search_text = 'basecolor'
 
             # Open the ZIP file
             with zipfile.ZipFile(model_filename_zip, 'r') as zip_ref:
+
                 # Iterate through the filenames in the ZIP archive
                 found_files = [file_name for file_name in zip_ref.namelist() if search_text.lower() in file_name.lower()]
                 
                 if found_files: # Find texture with basecolor in name
                     print(f"Files containing '{search_text}':")
                     for file in found_files:
-                        zip_ref.extract(file, "output_textures")
+                        zip_ref.extract(file, base_colors)
                         print(file)
 
-                        file_path = (f"output_textures/{file}")
+                        file_path = (f"{base_colors}/{file}")
 
                         color_thief = ColorThief(file_path)
                         dominant_color = color_thief.get_color(quality=1)
@@ -105,24 +110,19 @@ try:
                         # Dominant color of the image!
                         models_and_colors.append( (model_filename_glb, dominant_color) )
 
-                        # print(dominant_color)
-                    
-                    else:
-                        print(f"No files found containing '{search_text}' in their filename.")
+                else:
+                    print(f"No files found containing '{search_text}' in their filename.")
 
-                
-
-                # print(f"3D model saved as: {model_filename_zip}")
-                os.remove(model_filename_zip)
-                # print("delete zip")
-                # print("\n\n")
-
+            os.remove(model_filename_zip)
+            os.remove(model_filename_usdz)
         else:
-            print(f"Failed to download 3D model. HTTP Status: {model_response_glb.status_code}") # WHEN SOMETHING FAILS
+            print(f"Failed to download 3D model. HTTP Status usdz: {model_response_usdz.status_code}") 
+            print(f"Failed to download 3D model. HTTP Status glb: {model_response_glb.status_code}") 
 
     else:
-        print("\nNo 3D model URL found in response.")
+        print("No 3D model URL found in response.")
 
+    shutil.rmtree(base_colors)
     print(models_and_colors)
 
 
