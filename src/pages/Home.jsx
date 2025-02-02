748c7a15a8b00eb1dc4b6c8a2c6d0259dc45dde7
Cy4 <veschi@gmail.com>
2025-02-02 07:17:48 +0000

    Home jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,41 @@
+import "./../style/home.scss";
+
+const Home = () => {
+  return (
+    <div className="home-styling">
+      <section className="hero is-fullheight-with-navbar">
+        <div className="hero-body">
+          <div className="container is-flex is-align-items-center is-justify-content-center">
+            <div className="columns">
+              <div className="column is-flex is-align-items-center is-justify-content-center">
+                <div className="content pl-3 is-flex  is-align-items-center is-justify-content-center is-flex-direction-column" >
+                  <h1
+                    style={{
+                      fontSize: "6em",
+                      fontWeight: "bold",
+                      color: "black",
+                    }}
+                  >
+                    furn.it
+                  </h1>
+                  <p className="is-size-3">
+                    Your <strong>personalized</strong> interior designing
+                    assistant.
+                  </p>
+                  <a href="/form" className="button is-large">
+                    <span className="icon">
+                      {/* <FontAwesomeIcon icon={faRocket} /> */}
+                    </span>
+                    <span>Design your Interior</span>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Home;
