be79d0dfce3403c14630c61cade57a54bbc7e32d
hh2023 <helen.huang23@imperial.ac.uk>
2025-02-02 00:34:22 +0000

    feat: saves basecolor files

--- IKEA-api.py
+++ IKEA-api.py
@@ -1,9 +1,10 @@
 import ikea_api
 import requests
-from pygltflib import GLTF2
-import trimesh
 import os
 import base64
+import zipfile
+# import cv2
+import numpy
 
 # Constants like country, language, base url
 constants = ikea_api.Constants(country="gb", language="en")
@@ -26,9 +27,9 @@ pip_item = ikea_api.PipItem(constants)
 #print(pip_item.get_item(["30457903"]))
 
 rotera_item = ikea_api.RoteraItem(constants)
-rotera_item.get_item("00404204")
-
 item_code = "00404204"
+rotera_item.get_item(item_code)
+
 
 try:
     # Get the endpoint and execute it
@@ -42,26 +43,64 @@ try:
     model_urls = []
     for item in response.get("models"):
         cur_url = item.get("url")
-        if ".glb" in cur_url:
+        if ".usdz" in cur_url:
             model_urls.append(item.get("url"))
     for variation in response.get("variations"):
         for item in variation.get("models"):
             cur_url = item.get("url")
-            if ".glb" in cur_url:
+            if ".usdz" in cur_url:
                 model_urls.append(item.get("url"))
     print(model_urls[0])
     for i, model_url in enumerate(model_urls):
-        print(f"3D Model Found: {model_urls[0]}")
+        print(f"3D Model Found: {model_url}")
 
         # Download the model
-        model_response = requests.get(model_urls[0])
+        model_response = requests.get(model_url)
 
         if model_response.status_code == 200:
-            model_filename = (f"{i}.glb"
-                              f"")  # Adjust file format if needed
-            with open(model_filename, "wb") as file:
+            model_filename_usdz = (f"{i}.usdz"
+                                   f"")  # Adjust file format if needed
+            with open(model_filename_usdz, "wb") as file:
                 file.write(model_response.content)
-            print(f"3D model saved as: {model_filename}")
+            print(f"3D model saved as: {model_filename_usdz}")
+
+            
+            model_filename_zip = (f"{i}.zip"
+                                  f"")  # Adjust file format if needed
+            with open(model_filename_zip, "wb") as file:
+                file.write(model_response.content)
+
+            # Path to the ZIP file
+            search_text = 'basecolor'
+
+            # Open the ZIP file
+            with zipfile.ZipFile(model_filename_zip, 'r') as zip_ref:
+                # Iterate through the filenames in the ZIP archive
+                found_files = [file_name for file_name in zip_ref.namelist() if search_text.lower() in file_name.lower()]
+                
+                if found_files:
+                    print(f"Files containing '{search_text}':")
+                    for file in found_files:
+                        zip_ref.extract(file, "output_textures")
+                        print(file)
+                else:
+                    print(f"No files found containing '{search_text}' in their filename.")
+
+            
+
+            print(f"3D model saved as: {model_filename_zip}")
+            os.remove(model_filename_zip)
+            print("delete zip")
+
+
+            # myimg = cv2.imread('image.jpg')
+            # avg_color_per_row = numpy.average(myimg, axis=0)
+            # avg_color = numpy.average(avg_color_per_row, axis=0)
+            # print(avg_color)
+
+
+            print("\n\n")
+
         else:
             print(f"Failed to download 3D model. HTTP Status: {model_response.status_code}")
 
@@ -71,22 +110,3 @@ try:
 except Exception as e:
     print(f"Unexpected error: {e}")
 
-for i in range(len(model_urls)):
-
-    # Load the GLB file
-    mesh = trimesh.load_mesh(f'{i}.glb')
-
-    # Get the textures from the GLB
-    textures = mesh.visual.material.textures
-
-    # Save textures as images (optional)
-    if textures:
-        for idx, texture in enumerate(textures):
-            # Extract and save texture as image
-            image = texture.image
-            image.save(f"texture_{idx}.png")
-            print(f"Saved texture_{idx}.png")
-    else:
-        print("No textures found.")
-
-print(model_urls)
